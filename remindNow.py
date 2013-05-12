03d0d0b8e123c1b57430a3d2af73587909fd86db
bercio <lorenzo233@gmail.com>
2013-05-13 00:29:25 +0800

    la teoria c'e'

--- remindNow.py
+++ remindNow.py
@@ -6,25 +6,23 @@ Display the current and the next event from remind
 import time
 import subprocess
 remindFile = '/home/bercio/Reminders/all.rem'
-def getCalendar():
-    remindOutput = subprocess.check_output(['remind', '-s+1' '-b1', remindFile])
-    lsRemindOutput =  for i in remindOutput.splitlines():
-                            i = i.split()
 
-    shreddedRemindOutput = []
-    for i in lsRemindOutput:
-        if i[0] == time.strftime("%Y/%m/%d", time.localtime()):
-            shreddedRemindOutput += i
-    return shreddedRemindOutput
-def getEvents():
-    startandEndTimes = getCalendar()[5].strip(':').split('-')
-    for i, v in enumerate(getCalendar()):
-        if startandEndTimes[0] <= time.strftime("%H%M") <= startandEndTimes[1]:
-            eventNow = v
-            eventThen = getCalendat[(i+1)]
-            return (eventNow, eventNow)
-            exit
-        else:
-            return "Nothing else today"
-print(getEvents())
+def getDayEvents():
+    remindOutput = subprocess.check_output(['remind', '-s+1' '-b1', remindFile]).splitlines()
+    dayEvents = []
+    for lines in remindOutput:
+        lines = lines.split()
+        if lines[0] == time.strftime("%Y/%m/%d"):
+            dayEvents.append(lines)
+    return dayEvents
 
+for lines in getDayEvents():
+    #split  the field starttime-sendtime in the fields start-time and endtime.
+    lines[5].split('-')
+    lines.insert(5, lines[5][0])
+    lines[5] = lines[5][1]
+    if time.strptime(','.join(lines[0], lines[4])) < time.localtime() < time.strptime(''.join(lines[0], lines[5])):
+        nowEvent = lines
+        nextEvent = getDayEvents()[(getDayEvents().index(lines) + 1)]
+        exit
+print(nowEvent[6:-1], "|", nextEvent[6:-1])
